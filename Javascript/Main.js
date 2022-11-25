5f3a741b8cbe46135d3326258b4c21cdae0ecc15
Sarah020202 <101698360+Sarah020202@users.noreply.github.com>
2022-11-25 17:25:19 +0900

    dsgfs

---
+++ Javascript/Main.js
@@ -0,0 +1,11 @@
+function over1(obj) { obj.src = "../media/연땡땡black.png"; }
+function out1(obj) { obj.src = "../media/연땡땡color.png"; }
+
+function over2(obj) { obj.src = "../media/찌만니black.png"; }
+function out2(obj) { obj.src = "../media/찌만니color.png"; }
+
+function over3(obj) { obj.src = "../media/람셩이black.png"; }
+function out3(obj) { obj.src = "../media/람셩이color.png"; }
+
+function over4(obj) { obj.src = "../media/꾸리미black.png"; }
+function out4(obj) { obj.src = "../media/꾸리미color.png"; }
